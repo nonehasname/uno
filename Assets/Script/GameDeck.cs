b80b99112013cbc75833f84a1d91dae69e0b280e
Abdul-Muiz Yusuff <muiz.dev@gmail.com>
2022-04-23 16:13:18 -0700

    Added SetDeckFullness function to GameDeck script

--- Assets/Script/GameDeck.cs
+++ Assets/Script/GameDeck.cs
@@ -6,36 +6,39 @@ public class GameDeck : MonoBehaviour {
 
   private Sprite base_card_sprite_ = null;
   private Dictionary<float, Sprite> card_sprites_ = new Dictionary<float, Sprite>();
+  private SpriteRenderer renderer_;
+
+  public float fullness_ = 1.0f;
 
   void Start() {
-    SpriteRenderer renderer = GetComponent<SpriteRenderer>();
-    Debug.Assert(renderer != null);
-    Debug.Assert(renderer.sprite);
+    renderer_ = GetComponent<SpriteRenderer>();
+    Debug.Assert(renderer_ != null);
+    Debug.Assert(renderer_.sprite);
 
-    base_card_sprite_ = renderer.sprite;
+    base_card_sprite_ = renderer_.sprite;
     InitializeCardSpriteStates();
+  }
 
-    const int kSpriteIndex = 3;
-    Debug.Log("Sprite Count: " + card_sprites_.Count);
-    int ct = 0;
-    foreach (KeyValuePair<float, Sprite> entry in card_sprites_) {
-      if (ct == kSpriteIndex) {
-        Debug.Log("Sepecting sprite with fullness: " + entry.Key);
-        var sprite = entry.Value;
-
-        renderer.sprite = sprite;
-
-        break;
+  // @def Update the deck sprite to reflect the fullness of the deck.
+  public void SetDeckFullness(float fullness) {
+    fullness_ = Mathf.Clamp(fullness, 0.0f, 1.0f);
+    Sprite selected_sprite = null;
+    float best_fullness = -1;
+    foreach (var entry in card_sprites_) {
+      if (entry.Key <= fullness_ && entry.Key > best_fullness) {
+        selected_sprite = entry.Value;
+        best_fullness = entry.Key;
       }
-      ++ct;
     }
+    Debug.Assert(selected_sprite != null);
+    renderer_.sprite = selected_sprite;
   }
 
   // @desc Create different sprites for the card deck depending on how
   // many cards are left in the deck.
   // The less cards in the deck, the smaller the deck should look.
   private void InitializeCardSpriteStates() {
-    const int kFullnessStates = 4;
+    const int kFullnessStates = 8;
     for (int i = 0; i < kFullnessStates; ++i) {
       float fullness = ((float)i) / kFullnessStates;
       card_sprites_.Add(fullness, CreateDeckSprite(fullness));
@@ -47,7 +50,7 @@ public class GameDeck : MonoBehaviour {
   private Sprite CreateDeckSprite(float fullness) {
     Debug.Assert(base_card_sprite_ != null);
     fullness = Mathf.Clamp(fullness, 0.0f, 1.0f);
-    const int kThickness = 8;
+    const int kThickness = 12;
 
     int offset = (int)(kThickness * fullness);
     Texture2D base_tex = base_card_sprite_.texture;
@@ -71,7 +74,7 @@ public class GameDeck : MonoBehaviour {
     tex.Apply();
     // construct a sprite using this new texture
     // Set the pivot to the center fo rnow.
-    return Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+    return Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.0f));
   }
 
   private void PrintTexture(in Texture2D tex) {
