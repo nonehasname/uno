89ee1622e7c6960b2452b0120a7d147e6266e103
Abdul-Muiz Yusuff <muiz.dev@gmail.com>
2022-04-23 14:33:27 -0700

    Added game table script

--- Assets/Script/GameTable.cs
+++ Assets/Script/GameTable.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameTable : MonoBehaviour {
+
+  // Position where the cards should go when played onto the
+  // board.
+  private Vector3 card_play_stack_pos_ = new Vector3(0, 0, 0);
+  public Vector3 CardPlayStackPos {
+    get { return card_play_stack_pos_; }
+  }
+
   // Start is called before the first frame update
   void Start() {
 
