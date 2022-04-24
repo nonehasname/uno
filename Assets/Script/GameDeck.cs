8ad8c1c0291e75781f9108a91e3e5feb95fa0939
Abdul-Muiz Yusuff <muiz.dev@gmail.com>
2022-04-23 18:25:40 -0700

    Added drawable card to the deck

--- Assets/Script/GameDeck.cs
+++ Assets/Script/GameDeck.cs
@@ -8,15 +8,25 @@ public class GameDeck : MonoBehaviour {
   private Dictionary<float, Sprite> card_sprites_ = new Dictionary<float, Sprite>();
   private SpriteRenderer renderer_;
 
+  // The drawable card is the card that creates the card drawing
+  // illusion. Should be reset to its original position after 
+  // each draw animation.
+  private GameObject drawable_card_;
+  private Vector3 lowest_deck_pos_;
+  private Vector3 highest_deck_pos_;
+
   public float fullness_ = 1.0f;
+  private const int kDeckThickness = 12;
 
   void Start() {
     renderer_ = GetComponent<SpriteRenderer>();
     Debug.Assert(renderer_ != null);
     Debug.Assert(renderer_.sprite);
+    CreateDrawableCard();
 
     base_card_sprite_ = renderer_.sprite;
     InitializeCardSpriteStates();
+    SetDeckFullness(1.0f);
   }
 
   // @def Update the deck sprite to reflect the fullness of the deck.
@@ -31,6 +41,7 @@ public class GameDeck : MonoBehaviour {
       }
     }
     Debug.Assert(selected_sprite != null);
+    fullness_ = best_fullness;
     renderer_.sprite = selected_sprite;
   }
 
@@ -38,9 +49,8 @@ public class GameDeck : MonoBehaviour {
   // many cards are left in the deck.
   // The less cards in the deck, the smaller the deck should look.
   private void InitializeCardSpriteStates() {
-    const int kFullnessStates = 8;
-    for (int i = 0; i < kFullnessStates; ++i) {
-      float fullness = ((float)i) / kFullnessStates;
+    for (int i = 0; i < kDeckThickness; ++i) {
+      float fullness = ((float)i) / kDeckThickness;
       card_sprites_.Add(fullness, CreateDeckSprite(fullness));
     }
   }
@@ -50,9 +60,8 @@ public class GameDeck : MonoBehaviour {
   private Sprite CreateDeckSprite(float fullness) {
     Debug.Assert(base_card_sprite_ != null);
     fullness = Mathf.Clamp(fullness, 0.0f, 1.0f);
-    const int kThickness = 12;
 
-    int offset = (int)(kThickness * fullness);
+    int offset = (int)(kDeckThickness * fullness);
     Texture2D base_tex = base_card_sprite_.texture;
     Texture2D tex = new Texture2D(base_tex.width, base_tex.height + offset);
 
@@ -77,13 +86,43 @@ public class GameDeck : MonoBehaviour {
     return Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.0f));
   }
 
-  private void PrintTexture(in Texture2D tex) {
-    for (int i = 0; i < tex.width; ++i) {
-      string tex_str = "";
-      for (int j = 0; j < tex.height; ++j) {
-        tex_str += tex.GetPixel(i, j);
-      }
-      Debug.Log(tex_str);
-    }
+  private void CreateDrawableCard() {
+    // 0.321
+    drawable_card_ = new GameObject();
+    drawable_card_.name = "Drawable Card";
+    var r = drawable_card_.AddComponent<SpriteRenderer>();
+    r.transform.parent = transform.parent;
+
+    Sprite source_spr = GetComponent<SpriteRenderer>().sprite;
+    Sprite cpy_spr = Sprite.Create(
+      source_spr.texture,
+      new Rect(0.0f, 0.0f, source_spr.texture.width, source_spr.texture.height),
+      new Vector2(0.5f, 0.0f));
+    r.sprite = cpy_spr;
+
+    drawable_card_.transform.position = new Vector3(
+      transform.position.x,
+      transform.position.y,
+      -2);
+    drawable_card_.transform.rotation = transform.rotation;
+    drawable_card_.transform.localScale = transform.localScale;
+
+    lowest_deck_pos_ = drawable_card_.transform.position;
+    highest_deck_pos_ = drawable_card_.transform.position;
+    highest_deck_pos_.y += 0.325f;
+
+    UpdateDrawableCardPosition();
+    drawable_card_.SetActive(false);
+  }
+
+  private void UpdateDrawableCardPosition() {
+    Debug.Assert(drawable_card_ != null);
+    float diff = highest_deck_pos_.y - lowest_deck_pos_.y;
+    float delta = diff * fullness_;
+    drawable_card_.transform.position = new Vector3(
+      drawable_card_.transform.position.x,
+      lowest_deck_pos_.y + delta,
+      drawable_card_.transform.position.z
+    );
   }
 }
